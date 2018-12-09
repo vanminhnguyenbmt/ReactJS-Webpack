d6d85f653aca17f322c6e3e0f5b2758a436353dd
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-12-09 15:21:55 +0700

    Basic webpack
    Use entry, output

---
+++ LESSON01-WEBPACK-BASIC/src/index.js
@@ -0,0 +1,6 @@
+const sum = require('./math');
+
+const total = sum (3, 5);
+console.log(total);
+
+document.write(total);
---
+++ LESSON01-WEBPACK-BASIC/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const config = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
+
+module.exports = config;
